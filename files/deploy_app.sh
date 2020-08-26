6ec43e85c5c3ec076614ec2c9c58f27bf57ce891
Lars Pinnow <error08@users.noreply.github.com>
2020-08-26 15:16:51 +0200

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -12,6 +12,7 @@ cat << EOM > /var/www/html/index.html
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to Lars' app.
+  Test
   <!-- END -->
 
   </div>
